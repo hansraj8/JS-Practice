742346db97ed8b1012dadab0e981316ee337be1f
Hansraj sharma <105164338+hansraj8@users.noreply.github.com>
2023-11-26 01:44:16 +0530

    anagram solved

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -8,7 +8,15 @@
 */
 
 function isAnagram(str1, str2) {
+  let sorted1 = str1.split('').sort().join('');
+  let sorted2 = str2.split('').sort().join('');
 
+  if(sorted1 === sorted2){
+      return true;
+  }
+  else{
+      return false;
+  }
 }
 
 module.exports = isAnagram;
